cfef886f538dcaa40f6d22f728148c855e41ea60
maludo99 <darioschmid99@outlook.com>
2020-12-29 13:26:50 +0100

    Update initial.sh

--- initial.sh
+++ initial.sh
@@ -8,8 +8,8 @@ sudo apt-get update && sudo apt-get -y upgrade
  
 # Via https://gist.github.com/damoclark/ab3d700aafa140efb97e510650d9b1be
 # Execute the config options starting with 'do_' below
-grep -E -v -e '^\s*#' -e '^\s*$' <<END | \
-sed -e 's/$//' -e 's/^\s*/\/usr\/bin\/raspi-config nonint /' | bash -x -
+##grep -E -v -e '^\s*#' -e '^\s*$' <<END | \
+##sed -e 's/$//' -e 's/^\s*/\/usr\/bin\/raspi-config nonint /' | bash -x -
 #
  
 # Drop this file in SD card root. After booting run: sudo /boot/setup.sh
@@ -30,7 +30,7 @@ do_change_timezone Europe/Berlin
 do_change_locale LANG=de_DE.UTF-8
  
 # Don't add any raspi-config configuration options after 'END' line below & don't remove 'END' line
-END
+##END
 
 
 
